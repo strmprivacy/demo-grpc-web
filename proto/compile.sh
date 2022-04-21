607ff25e74af8027bba7ac79bd46bff46b1111ca
Jan-Kees van Andel <jan-kees@strmprivacy.io>
2022-04-21 15:04:23 +0200

    fix: mkdir before deleting files and some improvements

--- proto/compile.sh
+++ proto/compile.sh
@@ -1,4 +1,6 @@
-rm -rf ../src/proto_gen/*
-protoc -I=. *.proto \
-  --js_out=import_style=commonjs:../src/proto_gen \
-  --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:../src/proto_gen
+#!/bin/bash
+mkdir -p src/proto_gen
+rm -rf src/proto_gen/*
+protoc -I=. proto/grpc-demo_v1.proto \
+  --js_out=import_style=commonjs:src/proto_gen \
+  --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:src/proto_gen
